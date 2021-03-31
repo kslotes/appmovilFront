52cd2fb1b129b5137d1382bcdc04e65be7cb3264
kslotes <44619076+kslotes@users.noreply.github.com>
2021-03-31 17:09:47 -0300

    Segundo

---
+++ src/components/StyledTable/TableHeader.js
@@ -0,0 +1,12 @@
+export const TableHeader = () => {
+      return (
+            <thead>
+                  <tr>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Acciones</th>
+                  </tr>
+            </thead>
+      )
+}
---
+++ src/components/StyledTable/TdAcciones.js
@@ -0,0 +1,11 @@
+import * as Icon from 'react-bootstrap-icons';
+
+export const TdAcciones = () => {
+      return (
+            <td>
+                  <Icon.Eye className="mr-1 ml-1"/>
+                  <Icon.Pencil className="mr-1 ml-1"/>
+                  <Icon.Phone className="mr-1"/>
+            </td>
+      )
+}
